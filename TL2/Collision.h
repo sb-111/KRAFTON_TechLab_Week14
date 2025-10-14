206f491d5b266b04969fd12d5c02df17ebaa5e2b
Jinhyeok Choi <90018372+haedam19@users.noreply.github.com>
2025-10-15 01:55:30 +0900

    feat: Bounding sphere

--- TL2/Collision.h
+++ TL2/Collision.h
@@ -1,8 +1,13 @@
 ﻿#pragma once
 struct FAABB;
 struct FOBB;
+struct FBoundingSphere;
 
 namespace Collision
 {
     bool Intersects(const FAABB& Aabb, const FOBB& Obb);
+
+    bool Intersect(const FAABB& Aabb, const FBoundingSphere& Sphere);
+
+	bool Intersect(const FOBB& Obb, const FBoundingSphere& Sphere);
 }
